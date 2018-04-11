126ee6202f6bd7cfae7d0ae4d7adc5f6613d4a60
cjshinning <784319417@qq.com>
2018-04-11 22:18:52 +0800

    react基础知识state

--- src/App.js
+++ src/App.js
@@ -6,20 +6,36 @@ class App extends React.Component{
     return (
       <div>
         <h2>独立团,团长{boss}</h2>
-        <Yiying 老大='张大喵'></Yiying>
-        <Qilianbing 老大='孙德胜'></Qilianbing>
+        <Yiying laoda='张大喵'></Yiying>
+        <Qilianbing laoda='孙德胜'></Qilianbing>
       </div>
     )
   }
 }
 
 function Qilianbing(props){
-  return <h2>骑练兵连长{props.老大}，冲啊!</h2>
+  return <h2>骑练兵连长{props.laoda}，冲啊!</h2>
 }
 
 class Yiying extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      solders: ['虎子', '柱子', '王根生']
+    }
+  }
   render(){
-    return <h2>一营营长,{this.props.老大}</h2>
+    return (
+      <div>
+        <h2>一营营长,{this.props.laoda}</h2>
+        <ul>
+          {this.state.solders.map(v=>{
+            return <li key={v}>{v}</li>;
+          })}
+        </ul>
+      </div>
+    )
   }
 }
 
