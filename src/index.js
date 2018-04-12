8bbbbec1e49ca0860ad948fb0230469de2135e32
cjshinning <784319417@qq.com>
2018-04-12 10:43:44 +0800

    redux基础使用

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore}  from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function counter(state = 10, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+        return state + 1
+        case 'DECREMENT':
+        return state - 1
+        default:
+        return state
+    }
+}
+
+let store = createStore(counter)
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'DECREMENT' })
