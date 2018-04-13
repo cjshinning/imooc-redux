42590e7b6eebbbe2c6388fe59e019b7dc2f3d9e8
cjshinning <784319417@qq.com>
2018-04-13 15:44:03 +0800

    判断路由

--- server/server.js
+++ server/server.js
@@ -1,53 +1,8 @@
 var express = require('express');
-const mongoose = require('mongoose');
+const userRouter = require('./user')
 
-// 链接mongo 并且使用imooc这个集合
-const DB_URL = 'mongodb://localhost:27017/imooc';
-mongoose.connect(DB_URL);
-mongoose.connection.on('connected', function(){
-    console.log('mongo connect success');
-});
-
-const User = mongoose.model('user', new mongoose.Schema({
-    user: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    }
-}))
-// 新增数据
-// User.create({
-//     user: 'xiaohua',
-//     age: 12
-// }, function(err, doc){
-//     if(!err){
-//         console.log(doc);
-//     }else{
-//         console.log(err);
-//     }
-// })
-
-// 新建app
-// User.remove({age: 18}, function(err, doc){
-//     console.log(doc);
-// })
-// User.update({user: 'xiaoming'},{'$set': {age:26}},function(err, doc){
-//     console.log(doc)
-// })
 var app = express();
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>');
-});  
-
-app.get('/data', function (req, res) {
-    User.findOne({user: 'xiaoming'}, function(err,doc){
-        res.json(doc);
-    })
-    // res.json({ name: 'imooc React app!!!', type: 'IT' });
-});
+app.use('/user', userRouter)
 
 app.listen(9093, function () {
     console.log('Example app listening on port 9093!');
---
+++ src/component/authroute/authroute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+
+class AuthRoute extends React.Component{
+    componentDidMount(){
+        // 获取用户信息
+        axios.get('/user/info').
+            then(res=>{
+                if(res.status==200){
+                    console.log(res.data)
+                }
+            })
+
+        // 是否登录
+        // 现在的url地址 login是不需要跳转的
+        // 用户的type 身份是boss还是牛人
+        // 用户是否完善信息（选择头像 个人简介）
+    }
+
+    render(){
+        return <p>判断跳转</p>
+    }
+}
+
+export default AuthRoute
